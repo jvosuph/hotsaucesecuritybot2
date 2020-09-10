cd910492c4e1c6bf6ab67f03755159ec35f405f5
jvosuph <71087515+jvosuph@users.noreply.github.com>
2020-09-10 11:44:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,30 +27,3 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.NzUxOTM3MTEzMDI4Njg5OTUw.X1QWAg.YZp8DqTtiKI5IVeIG8yT01HHAkQ);//BOT_TOKEN is the Client Secret
-Step 3: Add a new file, named package.json containing the following:
-{
-
-  "name": "Example",
-
-  "description": "Example",
-
-  "version": "0.0.0",
-
-  "main": "bot.js",
-
-  "scripts": {
-
-    "start": "node bot.js"
-
-  },
-
-  "dependencies": {
-
-    "discord.js": "11.1.0",
-
-    "request": "2.81.0"
-
-  }
-
-}
-Step 4: Create a new file, named procfile containing the follo
